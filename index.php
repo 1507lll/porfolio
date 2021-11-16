458a6bc8668485f5adc8112e28062531edcccc34
1507lll <92914700+1507lll@users.noreply.github.com>
2021-11-16 20:57:46 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Aidana</title>
+  <link rel="stylesheet"  href="main.css">
+</head>
+<body>
+<div class="WholePort">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Mike_Tyson_2019_by_Glenn_Francis.jpg" alt="Me">
+  <p>Я Адинур мне 18 лет занимаюсь боксом <br>В свободное время люблю гулять и смотреть сериалы<br>
+  Aдрес - Кулиевой 85 <br>Номер- 0556 42 76 23<br></p>
+
+</div>
+<div class="Icons">
+
+</div>
+
+
+</body>
+</html>
